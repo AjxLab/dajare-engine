d1238edfee7222c0b63b2c9324aebc037dc3876b
AjxLab <abe12@mccc.jp>
2020-03-12 03:59:11 +0900

    add goo & jetrun

--- docomo.py
+++ docomo.py
@@ -1,5 +1,7 @@
 '''
 docomo API
+ - Powered by goo
+ - Powered by Jetrun
 '''
 
 import requests
@@ -9,15 +11,31 @@ import yaml
 
 yml_data = yaml.load(open('config/docomo.yml'))
 APIKEY = yml_data['key']
-print(APIKEY)
 
-def goo(joke, apikey):
-    ## -----*----- Powered by goo -----*----- ##
-    # 形態素解析
 
+def goo(joke):
+    ## -----*----- 形態素解析 -----*----- ##
+    url = "https://api.apigw.smt.docomo.ne.jp/gooLanguageAnalysis/v1/morph?APIKEY={}".format( APIKEY )
+    header = { 'Content-Type': 'application/json' }
+    data = { 'sentence': joke }
+
+    res = requests.post(url, headers=header, data=json.dumps(data))
+
+    return res
+
+
+
+def jetrun(joke):
+    ## -----*----- センシティブチェック -----*----- ##
     url = 'https://api.apigw.smt.docomo.ne.jp/truetext/v1/sensitivecheck?APIKEY={}'.format( APIKEY )
-    header = {'Content-Type': 'application/x-www-form-urlencoded'}
-    body = { "text": joke }
+    header = { 'Content-Type': 'application/x-www-form-urlencoded' }
+    body = { 'text': joke }
 
-    response = requests.post(url, headers=header, data=body)
+    res = requests.post(url, headers=header, data=body)
 
+    return res
+
+
+if __name__ == '__main__':
+    print(goo('布団が吹っ飛んだ'))
+    print(jetrun('布団が吹っ飛んだ'))
